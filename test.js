e0454a341b0b97c5c91c619cfdb51991fee47b31
Thomas Parisot <oncletom@users.noreply.github.com>
2019-11-27 15:29:31 +0100

    teiHeader > titleStmt

--- test.js
+++ test.js
@@ -9,3 +9,9 @@ test('asciidoctor', t => {
   const tei = asciidoctor.convert('= Title', { standalone: true, backend: 'tei' })
   t.regex(tei, /<TEI xmlns="http:\/\/www.tei-c.org\/ns\/1.0"/)
 })
+
+test('teiHeader > titleStmt', t => {
+  const tei = asciidoctor.convert('= Title: Subtitle', { standalone: true, backend: 'tei' })
+  t.regex(tei, /<title type="main">Title<\/title>/)
+  t.regex(tei, /<title type="sub">Subtitle<\/title>/)
+})
--- transforms.js
+++ transforms.js
@@ -4,7 +4,15 @@ module.exports = {
   document: ({ node }) => {
     return `<?xml version="1.0" encoding="UTF-8"?>
 <TEI xmlns="http://www.tei-c.org/ns/1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.tei-c.org/ns/1.0 http://lodel.org/ns/tei.openedition.1.5.2/tei.openedition.1.5.2.xsd">
-${node.getContent()}
+  <teiHeader>
+    <fileDesc>
+      <titleStmt>
+        <title type="main">${node.getDocumentTitle({partition: true}).getMain()}</title>
+        <title type="sub">${node.getDocumentTitle({partition: true}).getSubtitle()}</title>
+      </titleStmt>
+    </fileDesc>
+  </teiHeader>
+  ${node.getContent()}
 </TEI>`
   }
 }
